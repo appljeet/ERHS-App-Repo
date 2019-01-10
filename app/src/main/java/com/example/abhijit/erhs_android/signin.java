d319ebed20812ca2f67eaff1855de3c024cdeaf1
Abhijit <abhijitsingh.student@gmail.com>
2019-01-09 21:28:39 -0800

    Commit #4

--- app/src/main/java/com/example/abhijit/erhs_android/signin.java
+++ app/src/main/java/com/example/abhijit/erhs_android/signin.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class signin extends AppCompatActivity {
@@ -12,14 +14,14 @@ public class signin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        //fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_signin);
 
-
-        //Get rid of notification bar
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-
+        //gradient animation
         ConstraintLayout constraintLayout = findViewById(R.id.signin);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
         animationDrawable.setEnterFadeDuration(2000);
